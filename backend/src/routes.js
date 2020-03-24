a9581e190d48bd200cee895e1548d9f5848f3e38
Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
2020-03-24 19:06:24 -0300

    Rotas get e post incidents e criação do controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
 const OngControler = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
 routes.get('/ongs', OngControler.index);
 
 routes.post('/ongs', OngControler.create );
+
+routes.get('/incidents', IncidentController.index);
+
+routes.post('/incidents', IncidentController.create);
 
 module.exports = routes;
