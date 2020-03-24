56f022031e91e4bb7a14224ecb70a0426a451fb1
Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
2020-03-24 20:14:54 -0300

    Criação de rota e controller delete incident

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,11 +6,10 @@ const IncidentController = require('./controllers/IncidentController');
 const routes = express.Router();
 
 routes.get('/ongs', OngControler.index);
-
-routes.post('/ongs', OngControler.create );
+routes.post('/ongs', OngControler.create);
 
 routes.get('/incidents', IncidentController.index);
-
 routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
