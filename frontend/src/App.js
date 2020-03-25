f5d96edb4c4d52d27dddb036dc77643c0151051d
Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
2020-03-25 19:34:51 -0300

    Estudo de estados.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header';
 
 function App() {
+  const [ counter, setCounter ] = useState(0)
+
+  function increment(){
+    setCounter(counter + 1);
+  }
+
   return (
     <div className="App">
-      <Header>Semana Omnistack</Header>
+      <Header>Contador: { counter }</Header>
+      <button onClick={ increment }>Incrementa</button>
     </div>
   );
 }
