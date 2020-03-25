dd7fab808dcfb61ee8f1961878a0b5d5e43adf0f
Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
2020-03-24 21:38:23 -0300

    Criado rota e controler de login

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,8 +3,11 @@ const express = require('express');
 const OngControler = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngControler.index);
 routes.post('/ongs', OngControler.create);
