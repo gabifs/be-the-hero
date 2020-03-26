75b73bb4199406fc187562826accc97505b067f3
Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
2020-03-26 15:45:25 -0300

    Implementação do componente Routes

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function Incidents() {
   return (
-    <View />
+    <View>
+      <Text>Incidents</Text>
+    </View>
   );
 }
